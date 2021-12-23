53500d5c319f9d0c9a8af0056f99584a832b5361
Nayem <naeemislam13790@gmail.com>
2021-12-23 14:58:43 +0600

    'commited'

--- agency-app-django/contact/models.py
+++ agency-app-django/contact/models.py
@@ -3,21 +3,17 @@ from django.db import models
 
 class Top_contact(models.Model):
 
-
     title = models.CharField(max_length=25)
     address = models.CharField(max_length=50)
     def __str__(self):
         return self.title
 
-
 class Top_contact2(models.Model):
-
 
     title = models.CharField(max_length=25)
     email = models.CharField(max_length=50)
     def __str__(self):
         return self.title
-
 
 class Top_contact3(models.Model):
 
