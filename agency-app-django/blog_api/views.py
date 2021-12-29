853ac2809e8a9642b4323bcba6a1d8b616ce4014
Nayem <naeemislam13790@gmail.com>
2021-12-29 15:26:15 +0600

    'commited'

--- agency-app-django/blog_api/views.py
+++ agency-app-django/blog_api/views.py
@@ -1,18 +1,34 @@
+from django.http import request
 from blog.models import Post
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, BasePermission, SAFE_METHODS, DjangoModelPermissions
 from rest_framework.generics import ListAPIView,ListCreateAPIView,RetrieveAPIView,RetrieveDestroyAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
 # Create your views here.
 from .serializers import Post_serializer
 
+# #permissions keys
+# IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly,
+# DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
+
+
+
+#custom permission for edit post
+class PostUserWritePermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+
 class Post_api(ListCreateAPIView):
+    permission_classes = [IsAdminUser]
     queryset = Post.objects.all()
     serializer_class = Post_serializer
 
 
-class Post_detail(RetrieveDestroyAPIView):
+class Post_detail(RetrieveUpdateDestroyAPIView,PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = Post_serializer
-
-
-
