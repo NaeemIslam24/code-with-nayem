0a54d1bc5b81f9d62c90646d0a90598c73d7f135
Stack Pro <naeemislam13790@gmail.com>
2022-02-10 17:37:45 +0600

    commited

--- agency-app-django/test2/settings.py
+++ agency-app-django/test2/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'qo9_7qg01g3slqi)y%j_b18p8wuu6#3k-=tp6=19hn%@6zz3i4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codewithnayem.herokuapp.com', 'localhost', '127.0.0.1:8000/']
 
 
 # Application definition
