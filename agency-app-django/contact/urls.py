4fcf3787ee9b04fc9d0375cf1ce1b788d8aa6008
Nayem Nehal <53568454+NaeemIslam24@users.noreply.github.com>
2022-04-23 01:13:45 +0600

    Update urls.py

--- agency-app-django/contact/urls.py
+++ agency-app-django/contact/urls.py
@@ -4,10 +4,8 @@ urlpatterns = [
     path('', views.contact, name='contact'),
 
     # rest app
-<<<<<<< HEAD
+
     path('contact_api', views.Contact_api.as_view(), name='contact_api')
-=======
-    # path('', views.Contact_api.as_view(), name='contact')
->>>>>>> 5da3233635c387b653becb86a2c5da50ae42bf83
+
 ]
 
