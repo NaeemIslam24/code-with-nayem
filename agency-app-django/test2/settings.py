25bf85ad576b1b608f1e502686b947e06ad3df36
Nayem Nehal <53568454+NaeemIslam24@users.noreply.github.com>
2022-04-23 01:00:09 +0600

    Update settings.py

--- agency-app-django/test2/settings.py
+++ agency-app-django/test2/settings.py
@@ -169,10 +169,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-<<<<<<< HEAD
-EMAIL_HOST_USER = "nayemnehal.cu@gmail.com"
-<<<<<<< HEAD
-EMAIL_HOST_PASSWORD = 'nnnnssss$$$$'
+
+EMAIL_HOST_USER = ""
+
+EMAIL_HOST_PASSWORD = ''
 =======
 EMAIL_HOST_USER = "mail"
 EMAIL_HOST_PASSWORD = 'pass'
