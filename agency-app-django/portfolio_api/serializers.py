f8919d79c19bc1492952ae1f10cfcc23921f46d8
Stack Pro <naeemislam13790@gmail.com>
2022-05-21 05:53:55 +0600

    commited

--- agency-app-django/portfolio_api/serializers.py
+++ agency-app-django/portfolio_api/serializers.py
@@ -4,24 +4,46 @@ from rest_framework.reverse import reverse
 from portfolio.models import Portfolio, Category
 
 
-class Portfolio_serializer(serializers.ModelSerializer):
+# class Portfolio_serializer(serializers.ModelSerializer):
 
-    url = serializers.SerializerMethodField(read_only = True)
+#     url = serializers.SerializerMethodField(read_only = True)
+
+#     class Meta:
+#         model = Portfolio
+#         fields = ['url','title', 'client_name', 'project_date', 
+#         'Project_url', 'image1', 'image2', 'image3', 
+#         'image4','description', 'catagory']
+    
+    # def get_url(self, obj): 
+
+    #     request = self.context.get('request') 
+
+    #     if request is None:
+    #         return None
+
+    #     return reverse("retrive", kwargs={"title": obj.title, "pk": obj.pk}, request=request)
+
+class Catagory_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+
+class Portfolio_Serializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField()
+    catagory = Catagory_Serializer()
 
     class Meta:
         model = Portfolio
-        fields = ['url','title', 'client_name', 'project_date', 
-        'Project_url', 'image1', 'image2', 'image3', 
-        'image4','description', 'catagory']
-    
-    def get_url(self, obj): 
+        fields = (
+            'url', 'title', 'client_name', 'project_date', 'Project_url',
+            'image1', 'image2', 'image3', 'image4', 'description', 'catagory'
+        )
 
-        request = self.context.get('request') 
-
-        if request is None:
-            return None
-
-        return reverse("retrive", kwargs={"title": obj.title, "pk": obj.pk}, request=request)
+    def get_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri()
+        
 
 
 
--- agency-app-django/portfolio_api/views.py
+++ agency-app-django/portfolio_api/views.py
@@ -1,55 +1,87 @@
+# from rest_framework import generics
+# from portfolio.models import Portfolio
+# from . serializers import Portfolio_serializer
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+
+# class List(generics.ListAPIView):
+#     queryset = Portfolio.objects.all()
+#     serializer_class = Portfolio_serializer
+
+
+# class Retrive(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Portfolio.objects.all()
+#     serializer_class = Portfolio_serializer
+#     lookup_fields = [ "title", "pk"]
+
+
+
+# # class Category(APIView):
+
+# #     def get(self, request, formant=None):
+
+# #         # # print('catagory...........', catagory)
+# #         # test = request.user.id
+# #         # print('test...........', test)
+
+# #         category = request.GET['category']
+
+
+# #         queryset = Portfolio.objects.filter(catagory = category)
+        
+# #         serializers = Portfolio_serializer(queryset, many=True)
+
+# #         return Response(serializers.data)
+
+
+# class Category(generics.ListAPIView):
+
+#     queryset = Portfolio.objects.all()
+
+
+#     def list(self, request):
+       
+#         category = request.GET['category']
+#         queryset = self.get_queryset().filter(catagory=category)
+#         serializer = Portfolio_serializer(queryset, many=True)
+      
+#         return Response(serializer.data)
+    
+
+
 from rest_framework import generics
 from portfolio.models import Portfolio
-from . serializers import Portfolio_serializer
+from .serializers import  Portfolio_Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 class List(generics.ListAPIView):
     queryset = Portfolio.objects.all()
-    serializer_class = Portfolio_serializer
+    serializer_class = Portfolio_Serializer
 
 
 class Retrive(generics.RetrieveUpdateDestroyAPIView):
     queryset = Portfolio.objects.all()
-    serializer_class = Portfolio_serializer
-    lookup_fields = [ "title", "pk"]
+    serializer_class = Portfolio_Serializer
+    lookup_fields = ["title", "pk"]
 
 
+class Category(APIView):
 
-# class Category(APIView):
+    def get(self, request, formant=None):
+        category = request.query_params.get('category')
+        queryset = Portfolio.objects.filter(catagory=category)
+        serializers = Portfolio_Serializer(queryset, many=True, context={'request': request})
 
-#     def get(self, request, formant=None):
+        return Response(serializers.data)
 
-#         # # print('catagory...........', catagory)
-#         # test = request.user.id
-#         # print('test...........', test)
+# class Category(generics.ListAPIView):
 
-#         category = request.GET['category']
+#     lookup_fields =  "catagory"
 
-
-#         queryset = Portfolio.objects.filter(catagory = category)
-        
-#         serializers = Portfolio_serializer(queryset, many=True)
-
-#         return Response(serializers.data)
-
-
-class Category(generics.ListAPIView):
-
-    queryset = Portfolio.objects.all()
-
-
-    def list(self, request):
-       
-        category = request.GET['category']
-        queryset = self.get_queryset().filter(catagory=category)
-        serializer = Portfolio_serializer(queryset, many=True)
-      
-        return Response(serializer.data)
-    
-
-
-
+#     queryset = Portfolio.objects.all()
+#     serializer_class = Portfolio_serializer
 
     
 
